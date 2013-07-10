3126030bcf4f526334c56ca76453399bcf218483
Patrick Martin <pjmartin@ycp.edu>
2013-07-10 09:32:53 -0400

    Runge-Kutta now works with vector odes!

--- clojure/src/numerics/integration.clj
+++ clojure/src/numerics/integration.clj
@@ -21,13 +21,19 @@ interval t0 to tf."
   "A function that applies 4th order runge kutta to numerically integrate a first order differential equation of 
 the form xdot=f(x,t) over the interval t0 to tf."
   [diffeq x0 t0 tf delT]
+  (let [add (if (vz/vec? x0) ; check if the initial condition is a vectorz vector, or just a scalar!
+              vz/+
+              +)
+        mult (if (vz/vec? x0) ; check if the initial condition is a vectorz vector, or just a scalar!
+              vz/scale
+              *)]
   (loop [x x0 t t0 xs [] ts []]
     (if (> t tf)
       {:state xs :time ts}
       (let [h2 (* 0.5 delT)
             k1 (diffeq x t)
-            k2 (diffeq (+ x (* h2 k1)) (+ t h2))
-            k3 (diffeq (+ x (* h2 k2)) (+ t h2)) 
-            k4 (diffeq (+ x (* delT k3)) (+ t delT) ) ]
-        (recur (+ x (* (/ delT 6) (+ k1 (* 2 k2) (* 2 k3) k4))) (+ t delT) (conj xs x) (conj ts t) ))
-      )))
+            k2 (diffeq (add x (mult k1 h2)) (+ t h2))
+            k3 (diffeq (add x (mult k2 h2)) (+ t h2)) 
+            k4 (diffeq (add x (mult k3 delT)) (+ t delT) ) ]
+        (recur (add x (mult (add k1 (mult k2 2) (mult k3 2) k4) (/ delT 6)) ) (+ t delT) (conj xs x) (conj ts t) ))
+      ))))
--- clojure/test/numerics/core_test.clj
+++ clojure/test/numerics/core_test.clj
@@ -9,12 +9,20 @@
 (defn vector-ode [x t]
   (vz/vec [(+ (* -1 (first x)) (second x)) (* -1 (second x)) ]))
 
-(let [x0 (vz/vec [2 -1]) ; set initial condition to some vector
-      results (forward-euler vector-ode x0 0 1 0.01) ; solve numerically using Euler's method
+(time (let [x0 (vz/vec [2 -1]) ; set initial condition to some vector
+      results (forward-euler vector-ode x0 0 10 0.01) ; solve numerically using Euler's method
       x1 (map first (:state results)) ; extract the states
       x2 (map second (:state results))
       result-plot (xy-plot (:time results) x1)] ; now use Incanter to display our wonderful results...
   (do
     (add-lines result-plot (:time results) x2)
-    (view result-plot)))
-  
+    (view result-plot))))
+
+(time (let [x0 (vz/vec [3 -1]) ; set initial condition to some vector
+      results (forward-rk4 vector-ode x0 0 10 0.01) ; solve numerically using Euler's method
+      x1 (map first (:state results)) ; extract the states
+      x2 (map second (:state results))
+      result-plot (xy-plot (:time results) x1)] ; now use Incanter to display our wonderful results...
+  (do
+    (add-lines result-plot (:time results) x2)
+    (view result-plot))))
