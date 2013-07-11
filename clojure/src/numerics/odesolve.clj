05679be8ff3e7e12481e7da72da9ecc59acec83c
Patrick Martin <pjmartin@ycp.edu>
2013-07-11 15:33:27 -0400

    Switched to using the core.matrix api instead, but have vectorz as the implementation library.

--- clojure/src/numerics/odesolve.clj
+++ clojure/src/numerics/odesolve.clj
@@ -1,39 +1,28 @@
 (ns numerics.odesolve)
 
-(require '[mikera.vectorz.core :as vz])
+(require '[clojure.core.matrix :as mat])
+(mat/set-current-implementation :vectorz)
 
 (defn forward-euler
   "A function that numerically integrates a first order diffeq of the form xdot=f(x,t) forward in time over the 
 interval t0 to tf."
   [diffeq x0 t0 tf delT]
-  (let [add (if (vz/vec? x0) ; check if the initial condition is a vectorz vector, or just a scalar!
-              vz/+
-              +)
-        mult (if (vz/vec? x0) ; check if the initial condition is a vectorz vector, or just a scalar!
-              vz/scale
-              *)]
   (loop [x x0 t t0 xs [] ts []]
     (if (> t tf)
       {:state xs :time ts}
-      (recur (add x (mult (diffeq x t) delT)) (+ t delT) (conj xs x) (conj ts t))))))
+      (recur (mat/add x (mat/mul (diffeq x t) delT)) (+ t delT) (conj xs x) (conj ts t)))))
 
 (defn forward-rk4
   "A function that applies 4th order runge kutta to numerically integrate a first order differential equation of 
 the form xdot=f(x,t) over the interval t0 to tf."
   [diffeq x0 t0 tf delT]
-  (let [add (if (vz/vec? x0) ; check if the initial condition is a vectorz vector, or just a scalar!
-              vz/+
-              +)
-        mult (if (vz/vec? x0) ; check if the initial condition is a vectorz vector, or just a scalar!
-              vz/scale
-              *)]
   (loop [x x0 t t0 xs [] ts []]
     (if (> t tf)
       {:state xs :time ts}
       (let [h2 (* 0.5 delT)
             k1 (diffeq x t)
-            k2 (diffeq (add x (mult k1 h2)) (+ t h2))
-            k3 (diffeq (add x (mult k2 h2)) (+ t h2)) 
-            k4 (diffeq (add x (mult k3 delT)) (+ t delT) ) ]
-        (recur (add x (mult (add k1 (mult k2 2) (mult k3 2) k4) (/ delT 6)) ) (+ t delT) (conj xs x) (conj ts t) ))
-      ))))
+            k2 (diffeq (mat/add x (mat/mul k1 h2)) (+ t h2))
+            k3 (diffeq (mat/add x (mat/mul k2 h2)) (+ t h2)) 
+            k4 (diffeq (mat/add x (mat/mul k3 delT)) (+ t delT) ) ]
+        (recur (mat/add x (mat/mul (mat/add k1 (mat/mul k2 2) (mat/mul k3 2) k4) (/ delT 6)) ) (+ t delT) (conj xs x) (conj ts t) ))
+      )))
--- clojure/test/numerics/core_test.clj
+++ clojure/test/numerics/core_test.clj
@@ -1,15 +1,18 @@
 (ns numerics.integration-test
   (:use clojure.test
-        numerics.integration))
+        numerics.odesolve))
 
-(require '[mikera.vectorz.core :as vz])
 (use '(incanter core charts))
 
 ; Here we simulate the vector differential equation dot{x}_1 = -x_1 + x_2; dot{x}_2 = -x_2:
 (defn vector-ode [x t]
-  (vz/vec [(+ (* -1 (first x)) (second x)) (* -1 (second x)) ]))
+  (vec [(+ (* -1 (first x)) (second x)) (* -1 (second x)) ]))
 
-(time (let [x0 (vz/vec [2 -1]) ; set initial condition to some vector
+(defn scalar-ode [x t]
+  (* -0.8 x))
+
+; vector ode integration ( 2 variables)
+(time (let [x0 (vec [2 -1]) ; set initial condition to some vector
       results (forward-euler vector-ode x0 0 10 0.01) ; solve numerically using Euler's method
       x1 (map first (:state results)) ; extract the states
       x2 (map second (:state results))
@@ -18,7 +21,7 @@
     (add-lines result-plot (:time results) x2)
     (view result-plot))))
 
-(time (let [x0 (vz/vec [3 -1]) ; set initial condition to some vector
+(time (let [x0 (vec [3 -1]) ; set initial condition to some vector
       results (forward-rk4 vector-ode x0 0 10 0.01) ; solve numerically using Euler's method
       x1 (map first (:state results)) ; extract the states
       x2 (map second (:state results))
@@ -26,3 +29,29 @@
   (do
     (add-lines result-plot (:time results) x2)
     (view result-plot))))
+
+; scalar ode integration
+(defn scalar-ode [x t]
+  (* -0.8 x))
+
+(time (let [x0 2 ; set initial condition to some vector
+            results (forward-euler scalar-ode x0 0 4 0.01) ; solve numerically using Euler's method
+            x1 (:state results) ; extract the states
+            result-plot (xy-plot (:time results) x1)] ; now use Incanter to display our wonderful results...
+        (view result-plot)))
+
+(time (let [x0 2 ; set initial condition to some vector
+            results (forward-rk4 scalar-ode x0 0 4 0.01) ; solve numerically using Euler's method
+            x1 (:state results) ; extract the states
+            result-plot (xy-plot (:time results) x1)] ; now use Incanter to display our wonderful results...
+        (view result-plot)))
+
+; plot the graphs to compare accuracy 
+(let [x0 2
+      euler-results (forward-euler scalar-ode x0 0 4 0.01)
+      rk4-results (forward-rk4 scalar-ode x0 0 4 0.01)
+      result-plot (xy-plot (:time euler-results) (:state euler-results))]
+  (do 
+    (add-lines result-plot (:time rk4-results) (:state rk4-results))
+    (view result-plot)))
+      
